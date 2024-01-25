28084d2997409d075056024c0a1ffdcba259afb6
New Creative <ianparra200@gmail.com>
2024-01-25 02:40:35 -0300

    refactor: move components to $lib

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,27 +1,33 @@
-<script>
-    import { clientController } from '$lib/controllers/ClientController';
+<script lang="ts">
+  import { clientController } from '$lib/controllers/ClientController';
+  import { _ } from 'svelte-i18n';
+  let loading = false;
+  let success: string | null = null;
+  let credentials = {
+    email: '',
+    password: ''
+  };
 
-  let email = '';
-  let password = '';
-
-  import { _ } from 'svelte-i18n'
-
-  function handleLogin(){
-    console.log(email);
-    console.log(password);
-    clientController.login({email, password}).then(()=>{
+  function onSubmit(){
+    loading = true;
+    clientController.login(credentials).then(()=>{
+      success = credentials.email;
       console.log(clientController.getActiveSession());
     })
   }
 </script>
 
+{#if success}
+  <div></div>
+{/if}
+
 <div class="container">
   <h1 class="title">{$_('login.title')}</h1>
-  <form class="login-form" on:submit={handleLogin}>
+  <form class="login-form" on:submit|preventDefault={onSubmit}>
     <div class="form-label">{$_('login.email')}</div>
-    <input name="email" class="form-input" type="email" bind:value={email} placeholder={$_('login.enter.email')} required />
+    <input name="email" class="form-input" type="email" bind:value={credentials.email} placeholder={$_('login.enter.email')} required />
     <div class="form-label">{$_('login.password')}</div>
-    <input name="password" class="form-input" type="password" bind:value={password} placeholder={$_('login.enter.password')} required />
-    <button class="form-btn" type="submit">Iniciar Sesión</button>
+    <input name="password" class="form-input" type="password" bind:value={credentials.password} placeholder={$_('login.enter.password')} required />
+    <button class="form-btn" type="submit">{$_('login.title')}</button>
   </form>
 </div>
