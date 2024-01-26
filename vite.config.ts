a2f61f55bddf73c0e2083de141460eb394dc1e5a
MrChileball <chileball5@gmail.com>
2024-01-26 00:54:32 -0300

    fixed .env variables for vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,30 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import envSetup from './vite-env-setup'; // Ruta al archivo que configuraste en el paso 2
 
 export default defineConfig({
-	server: {
-		host: 'local.revolt.chat',
-		port: 5000,
-		proxy: {
-			'/api': {
-				target: import.meta.env.API_TARGET,
-				headers: {'Content-Type': 'application/json'},
-				rewrite: (path) => path.replace(/^\/api/, ''),
-			},
-			'/autumn': {
-				target: import.meta.env.AUTUMN_TARGET,
-				rewrite: (path) => path.replace(/^\/autumn/, ''),
-			},
-			'/january': {
-				target: import.meta.env.JANUARY_TARGET,
-				rewrite: (path) => path.replace(/^\/january/, ''),
-			},
-			'/ws': {
-				ws: true,
-				target: import.meta.env.WS_TARGET,
-			}
-		}
-	},
-	plugins: [sveltekit()]
+  server: {
+    host: 'local.revolt.chat',
+    port: 5000,
+    proxy: {
+      '/api': {
+        target: envSetup.VITE_API_TARGET,
+        headers: { 'Content-Type': 'application/json' },
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      '/autumn': {
+        target: envSetup.VITE_AUTUMN_TARGET,
+        rewrite: (path) => path.replace(/^\/autumn/, ''),
+      },
+      '/january': {
+        target: envSetup.VITE_JANUARY_TARGET,
+        rewrite: (path) => path.replace(/^\/january/, ''),
+      },
+      '/ws': {
+        ws: true,
+        target: envSetup.VITE_WS_TARGET,
+      },
+    },
+  },
+  plugins: [sveltekit()],
 });
