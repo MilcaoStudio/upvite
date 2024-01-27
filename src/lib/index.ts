a74f2383db1fbc9f7da0b4da49d7cb6cf3d786d7
New Creative <ianparra200@gmail.com>
2024-01-26 22:43:51 -0300

    fix: ...

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -21,27 +21,6 @@ export function mapToRecord<K extends symbol | string | number, V>(
     return record
 }
 
-/**
- * Inject a key into the window's globals.
- * @param key Key
- * @param value Value
- */
-export function injectWindow(key: string, value: any) {
-    (window as any)[key] = value;
-}
-
-/**
- * Inject a controller into the global controllers object.
- * @param key Key
- * @param value Value
- */
-export function injectController(key: string, value: any) {
-    (window as any).controllers = {
-        ...((window as any).controllers ?? {}),
-        [key]: value,
-    };
-}
-
 export function takeError(error: any): string {
     if (error.response) {
         const type = error.response.data?.type;
