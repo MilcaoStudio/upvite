6424606a7121ab6862a2cf655b83793e64bc1e8c
New Creative <ianparra200@gmail.com>
2024-02-19 17:42:58 -0300

    fix: CheckAuth renders after state

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,35 @@
 <script lang="ts">
-    import { browser } from '$app/environment';
-    import { page } from '$app/stores';
-    import { state } from '$lib/State';
-    import Binder from '$lib/components/context/Binder.svelte';
-    import Locale from '$lib/components/context/Locale.svelte';
-    import ModalRenderer from '$lib/components/modals/ModalRenderer.svelte';
-    import CheckAuth from '$lib/controllers/CheckAuth.svelte';
-    import '../styles/app.css'
-    import 'fluent-svelte/theme.css'
+    import { browser } from "$app/environment";
+    import { page } from "$app/stores";
+    import { state } from "$lib/State";
+    import Binder from "$lib/components/context/Binder.svelte";
+    import Locale from "$lib/components/context/Locale.svelte";
+    import Preloader from "$lib/components/indicators/Preloader.svelte";
+    import ModalRenderer from "$lib/components/modals/ModalRenderer.svelte";
+    import CheckAuth from "$lib/controllers/CheckAuth.svelte";
+    import "../styles/app.css";
+    import "fluent-svelte/theme.css";
 
+    let ready = false;
     if (browser) {
-	    state.hydrate().then(()=>console.log('State is READY!'));
+        state.hydrate().then(() => (ready = true));
     }
-    
 </script>
-<Locale>
-    {#if $page.url.pathname.startsWith('/login')}
-        <CheckAuth>
-            <slot />
-        </CheckAuth>
-    {:else}
-        <CheckAuth auth>
-            <slot />
-        </CheckAuth>
-    {/if}
-    <Binder />
-    <ModalRenderer />
-</Locale>
+
+{#if ready}
+    <Locale>
+        {#if $page.url.pathname.startsWith("/login")}
+            <CheckAuth>
+                <slot />
+            </CheckAuth>
+        {:else}
+            <CheckAuth auth>
+                <slot />
+            </CheckAuth>
+        {/if}
+        <Binder />
+        <ModalRenderer />
+    </Locale>
+{:else}
+    <Preloader type="spinner" />
+{/if}
