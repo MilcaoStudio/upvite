7e7c1becbf82634d50093ae6d3be76a6d0a4e7e1
New Creative <ianparra200@gmail.com>
2024-02-20 21:58:16 -0300

    fix: /invite route

---
+++ src/routes/invite/[code]/+page.svelte
@@ -0,0 +1,133 @@
+<script lang="ts">
+    import { goto, pushState } from "$app/navigation";
+    import { takeError } from "$lib";
+    import { state } from "$lib/State.js";
+    import Category from "$lib/components/atoms/Category.svelte";
+    import Error from "$lib/components/atoms/Error.svelte";
+    import Preloader from "$lib/components/indicators/Preloader.svelte";
+    import {
+        useClient,
+        useSession,
+    } from "$lib/controllers/ClientController.js";
+    import { Button } from "fluent-svelte";
+    import { API } from "revolt.js";
+    import { BxArrowBack } from "svelte-boxicons";
+    import { _ } from "svelte-i18n";
+    import "../../../styles/invite.css"
+    import TextSvelte from "$lib/i18n/TextSvelte.svelte";
+
+    export let data;
+
+    const code = data.code;
+    const session = useSession();
+    const client = useClient();
+    const layout = state.layout;
+
+    let processing = false;
+    let error: string;
+    let invite: API.InviteResponse;
+
+    $: {
+        if (!invite) {
+            client
+                .fetchInvite(code)
+                .then((data) => (invite = data))
+                .catch((err) => (error = takeError(err)));
+        }
+    }
+</script>
+
+{#if !invite}
+    <div class="preloader">
+        <!--<RequiresOnline>-->
+        {#if error}
+            <div class="invite" style:width="100%" style:height="100%">
+                <div class="details">
+                    <h1>{$_("app.special.invite.invalid")}</h1>
+                    <h2>{$_("app.special.invite.invalid_desc")}</h2>
+                    <div style:cursor="pointer">
+                        <Button variant="standard">
+                            <BxArrowBack
+                                size={32}
+                                on:click={() =>
+                                    pushState(layout.getLastPath(), {})}
+                            />
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        {:else}
+            <Preloader type="spinner" />
+        {/if}
+        <!--</RequiresOnline>-->
+    </div>
+{:else if invite.type == "Group"}
+    <!--TODO: Implement group invite-->
+    <h1>Unimplemented!</h1>
+{:else if invite.type == "Server"}
+    <div
+        class="invite"
+        style:background={invite.server_banner
+            ? `url('${client.generateFileURL(invite.server_banner)}')`
+            : null}
+    >
+        <div class="leave">
+            <BxArrowBack
+                size={32}
+                on:click={() => goto(layout.getLastPath())}
+            />
+        </div>
+
+        {#if !processing}
+            <div class="icon">
+                <!--ServerIcon-->
+            </div>
+        {/if}
+
+        <div class="details">
+            {#if processing}
+                <Preloader type="ring" />
+            {:else}
+                <h1>{invite.server_name}</h1>
+                <h2>
+                    #{invite.channel_name} •{" "}
+                    {$_("app.special.invite.user_count", {
+                        values: { member_count: invite.member_count },
+                    })}
+                </h2>
+                <h3>
+                    {$_("app.special.invite.invited_by", {
+                        values: { user: invite.user_name },
+                    })}
+                </h3>
+                <Category><Error {error} /></Category>
+                <Button
+                    variant="standard"
+                    on:click={async () => {
+                        if (!session) {
+                            return goto("/");
+                        }
+                        processing = true;
+                        try {
+                            if (invite.type != 'Server') {
+                                throw TypeError('Invite has invalid format')
+                            }
+                            await client.joinInvite(invite);
+                            goto(`/server/${invite.server_id}/channel/${invite.channel_id}`);
+                        } catch (err) {
+                            error = takeError(err);
+                        } finally {
+                            processing = false;
+                        }
+                    }}
+                >
+                {#if session}
+                    {$_('app.special.invite.accept')}
+                {:else}
+                    {$_('app.special.invite.login')}
+                {/if}
+                </Button>
+            {/if}
+        </div>
+    </div>
+{/if}
