6256835c8c46f9a1fc894120b382c92b92038374
MrChileball <chileball5@gmail.com>
2024-03-02 23:27:20 -0300

    Blablabla Settings Components

---
+++ src/lib/components/settings/common/scroller.svelte
@@ -0,0 +1,29 @@
+<div class="scroller-y">
+    <div class="container">
+        <slot/>
+    </div>
+</div>
+
+    
+
+
+<style>
+
+    .container{
+        min-width: 218px;
+        padding: 80px 8px;
+        display: flex;
+        gap: 2px;
+        flex-direction: column;
+
+    }
+
+    .scroller-y{
+
+        overflow-y: scroll;
+        transition: visibility .1s;
+
+    }
+
+</style>
+
--- src/routes/settings/[setting]/+page.svelte
+++ src/routes/settings/[setting]/+page.svelte
@@ -1,33 +1,122 @@
 <script>
+    import UprisingApp from "$lib/components/UprisingApp.svelte";
+    import Item from "$lib/components/settings/common/item.svelte";
+    import Scroller from "$lib/components/settings/common/scroller.svelte";
+
 export let data;
 
-    /**
-     * @type {any}
-     */
-let setting = data.setting;
 
     /**
-     * @type {typeof import("$lib/components/settings/appearance.svelte").default}
+     * @type {typeof import("$lib/components/settings/personal/appearance.svelte").default}
      */
 let component;
 
+let href = "/settings/"
+
+$: setting = data.setting;
+
 $: {
     switch (setting) {
-        case 'valor1':
-        case 'valor2':
-            import('$lib/components/settings/appearance.svelte').then(module => {
+        case 'support':
+        case 'appearance':
+            import('$lib/components/settings/personal/appearance.svelte').then(module => {
                 component = module.default;
             });
             break;
         default:
-            import('$lib/components/settings/profile.svelte').then(module => {
+            import('$lib/components/settings/personal/profile.svelte').then(module => {
                 component = module.default;
             });
     }
 }
 </script>
 
-<h1>{setting}</h1>
 
+<UprisingApp>
 
-<svelte:component this={component} />
+    <div class="flex-column">
+        <Scroller>
+            <h1>{setting}</h1>
+
+        <Item href="{href}appearance"><h>Appearance</h> </Item>
+    
+        
+        <a href="{href}appereance">
+            <h2>profile</h2>
+        </a>
+        <a href="{href}support">
+            <h2>profile</h2>
+        </a>
+        <a href="{href}appereance">
+            <h2>profile</h2>
+        </a>
+        <a href="{href}support">
+            <h2>profile</h2>
+        </a>
+        <a href="{href}appereance">
+            <h2>profile</h2>
+        </a>
+        <a href="{href}support">
+            <h2>profile</h2>
+        </a>
+        <a href="{href}appereance">
+            <h2>profile</h2>
+        </a>
+        <a href="{href}support">
+            <h2>profile</h2>
+        </a>
+        <a href="{href}appereance">
+            <h2>profile</h2>
+        </a>
+        <a href="{href}support">
+            <h2>profile</h2>
+        </a>
+        <a href="{href}support">
+            <h2>profile</h2>
+        </a>
+        <a href="{href}appereance">
+            <h2>profile</h2>
+        </a>
+        <a href="{href}support">
+            <h2>profile</h2>
+        </a>
+        <a href="{href}support">
+            <h2>profile</h2>
+        </a>
+        <a href="{href}appereance">
+            <h2>profile</h2>
+        </a>
+        <a href="{href}support">
+            <h2>profile</h2>
+        </a>
+    
+        </Scroller>
+        
+        <Scroller>
+            <svelte:component this={component} />
+        </Scroller>
+    
+        
+
+    </div>
+
+    
+    
+</UprisingApp>
+
+<style>
+    .flex-column{
+        display: flex;
+        flex-direction: row;
+        
+    }
+
+    a, a:link, a:visited, h2, p, h3{
+        color:var(--tertiary-foreground);
+    }
+
+    a:hover, h2:hover, h3, p:hover{
+        color: var(--primary-foreground);
+    }
+</style>
+
