6c91f957071731ed69380e069b99408b8c1ed657
MrChileball <chileball5@gmail.com>
2024-03-07 18:00:59 -0300

    animation an fixed

--- src/lib/components/modals/UserProfile.svelte
+++ src/lib/components/modals/UserProfile.svelte
@@ -5,12 +5,13 @@ import CategoryButton from "../form/CategoryButton.svelte";
 import UserIcon from "../user/UserIcon.svelte";
 
 
-let icon = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-flag-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 14h14v-9h-14v16" /></svg>`;
+
 
     
 </script>
 
-<CategoryButton {icon}></CategoryButton>
+
+
 
 <div class="base">
     <div class="container">
@@ -23,6 +24,7 @@ let icon = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon
                 </div>
                 <div class="medals"></div>
                 <div class="tabs">
+                    
     
                 </div>
     
