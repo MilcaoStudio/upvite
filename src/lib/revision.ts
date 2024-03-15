fc97af6c1662967019848a6f95b9608e1671c00f
New Creative <ianparra200@gmail.com>
2024-03-15 01:34:25 -0300

    feat: first official version

---
+++ src/lib/revision.ts
@@ -0,0 +1,4 @@
+export const REPO_URL: string = "https://github.com/MilcaoStudio/upvite/commit",
+    GIT_REVISION: string = "__GIT_REVISION__",
+    GIT_BRANCH: string = "__GIT_BRANCH__",
+    APP_VERSION = "__APP_VERSION__";
