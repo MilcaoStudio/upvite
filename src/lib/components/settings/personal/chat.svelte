3829fd97f73879f8e4f61dc1849eaa1d9b93b0ae
New Creative <ianparra200@gmail.com>
2024-04-06 22:15:30 -0300

    refactor: move network settings to chat tab

--- src/lib/components/settings/personal/chat.svelte
+++ src/lib/components/settings/personal/chat.svelte
@@ -1,6 +1,21 @@
 <script>
+    import { state } from "$lib/State";
+    import H3 from "$lib/components/atoms/heading/H3.svelte";
     import InDevelopment from "$lib/components/atoms/inDevelopment.svelte";
+    import { Checkbox } from "fluent-svelte";
+    
+    let media = state.network.media;
+    let shrinkMedia = media?.shrinkMedia;
+    let autoplay = media?.autoplay;
+    // make a slider instead?
+    let lessMessages = state.network.channel.messagesLimit < 50;
+    $: state.network.set("channel", {messagesLimit: lessMessages ? 30 : 100});
+    $: state.network.set("media", {shrinkMedia, autoplay});
 
 </script>
 
 <InDevelopment></InDevelopment>
+<H3>Ahorro de datos</H3>
+<Checkbox bind:checked={shrinkMedia}>Reducir tamaño de multimedia </Checkbox>
+<Checkbox bind:checked={autoplay}>Reproducir GIFs automáticamente</Checkbox>
+<Checkbox bind:checked={lessMessages}>Reducir descarga de mensajes</Checkbox>
