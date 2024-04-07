e7bf1e48500b980b3e4667fcc138d9e77fff19c9
New Creative <ianparra200@gmail.com>
2024-04-07 14:56:10 -0400

    feat: font selector

--- src/lib/components/settings/personal/appearance.svelte
+++ src/lib/components/settings/personal/appearance.svelte
@@ -1,8 +1,11 @@
-<script>
+<script lang="ts">
     import InDevelopment from "$lib/components/atoms/inDevelopment.svelte";
     import CustomCss from "./CustomCss.svelte";
-
+    import FontSelector from "./FontSelector.svelte";
+    import MonoFontSelector from "./MonoFontSelector.svelte";
 </script>
 
 <InDevelopment></InDevelopment>
+<FontSelector />
+<MonoFontSelector />
 <CustomCss />
