e10f8f80a11b2709598a47f157d97fdc434ca370
New Creative <ianparra200@gmail.com>
2024-04-07 23:31:38 -0400

    feat: friends page improved

--- src/lib/components/social/Friend.svelte
+++ src/lib/components/social/Friend.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
     import { goto } from "$app/navigation";
-    import { useClient } from "$lib/controllers/ClientController";
     import type { User } from "revolt.js";
+    import Plus from "svelte-boxicons/BxPlus.svelte";
+    import X from "svelte-boxicons/BxX.svelte";
+    import Envelope from "svelte-boxicons/BxEnvelope.svelte";
     import { modalController } from "../modals/ModalController";
     import UserIcon from "../user/UserIcon.svelte";
     import Username from "../user/Username.svelte";
+    import IconButton from "../atoms/input/IconButton.svelte";
 
-    const client = useClient();
     export let user: User;
 </script>
 
@@ -14,7 +16,25 @@
     <UserIcon target={user} size={60} onClick={()=>modalController.push({type: "user_profile", user_id: user._id})}/>
     <Username {user} />
     <div class="Actions">
-        <button on:click={()=>goto(`/open/${user._id}`)}>Send message</button>
+        <IconButton shape="circle" onClick={()=>{
+            user.relationship == "Friend"
+                ? modalController.push({
+                    type: "unfriend_user",
+                    target: user,
+                })
+                : user.removeFriend()
+        }}>
+            <X size={24} /> 
+         </IconButton>
+        {#if user.relationship == "Incoming"}
+        <IconButton shape="circle" onClick={()=>user.addFriend()}>
+            <Plus size={24} />
+        </IconButton>
+        {:else if user.relationship == "Friend"}
+        <IconButton shape="circle" onClick={()=>goto(`/open/${user._id}`)}>
+            <Envelope size={24} />
+        </IconButton>
+        {/if}
     </div>
 </div>
 
@@ -22,8 +42,12 @@
     .Friend {
         border: 1px solid var(--secondary-foreground);
         border-radius: var(--border-radius);
-        padding: 4px;
+        padding: 12px;
         display: flex;
         flex-direction: column;
+        text-align: center;
+    }
+    .Friend > :global(*) {
+        margin: auto;
     }
 </style>
--- src/lib/components/social/Friends.svelte
+++ src/lib/components/social/Friends.svelte
@@ -1,37 +1,160 @@
 <script lang="ts">
     import { useClient } from "$lib/controllers/ClientController";
     import { autorun } from "mobx";
-    import H3 from "../atoms/heading/H3.svelte";
+    import PageHeader from "../atoms/PageHeader.svelte";
     import Friend from "./Friend.svelte";
-    import Incoming from "./Incoming.svelte";
+    import UserDetail from "svelte-boxicons/BxUser.svelte";
+    import UserPlus from "svelte-boxicons/BxUserPlus.svelte";
+    import MessageAdd from "svelte-boxicons/BxMessageAdd.svelte";
+    import Tooltip from "../atoms/Tooltip.svelte";
+    import IconButton from "../atoms/input/IconButton.svelte";
+    import CollapsibleSection from "../CollapsibleSection.svelte";
+    import { t } from "svelte-i18n";
+    import { modalController } from "../modals/ModalController";
+    import { isTouchscreenDevice } from "$lib";
 
     const client = useClient();
     let users = [...client.users.values()];
-    $: autorun(()=>{
+    $: autorun(() => {
         users = [...client.users.values()];
     });
     $: users.sort((a, b) => a.username.localeCompare(b.username));
-    //$: friends = users.filter(x => x.relationship == "Friend");
-    let friends = [client.user!, client.user!, client.user!, client.user!];
-    $: incoming = users.filter(x => x.relationship == "None");
-    $: outgoing = users.filter(x => x.relationship == "Outgoing");
-
+    $: friends = users.filter((x) => x.relationship == "Friend");
+    $: online = friends.filter((x) => x.online && x.status?.presence != "Invisible");
+    $: offline = friends.filter((x) => !x.online || x.status?.presence == "Invisible");
+    $: incoming = users.filter((x) => x.relationship == "Incoming");
+    $: outgoing = users.filter((x) => x.relationship == "Outgoing");
+    $: isEmpty =
+        [friends, incoming, outgoing].reduce(
+            (p: number, n) => p + n.length,
+            0,
+        ) == 0;
 </script>
-<H3>Solicitudes</H3>
-<div class="UserGrid">
-    {#each incoming as user}
-        <Incoming {user} />
-    {/each}
-</div>
 
-<H3>Amigos</H3>
-<div class="UserGrid">
-    {#each friends as user}
-        <Friend {user} />
-    {/each}
+<PageHeader icon={UserDetail} withTransparency noBurger>
+    <div class="title">
+        {$t("app.navigation.tabs.friends")}
+    </div>
+    <div class="actions">
+        <Tooltip content="Create Group" placement="bottom">
+            <IconButton
+                onClick={() =>
+                    modalController.push({
+                        type: "create_group",
+                    })}
+            >
+                <MessageAdd size={24} />
+            </IconButton>
+        </Tooltip>
+        <Tooltip content="Add Friend" placement="bottom">
+            <IconButton
+                onClick={() =>
+                    modalController.push({
+                        type: "add_friend",
+                    })}
+            >
+                <UserPlus size={27} />
+            </IconButton>
+        </Tooltip>
+    </div>
+</PageHeader>
+<div data-scroll-offset="true" data-avoids-navigation="true">
+    <div
+        class="grid with-padding"
+        data-empty={isEmpty}
+        data-mobile={isTouchscreenDevice}
+    >
+        {#if isEmpty}
+            {$t("app.special.friends.nobody")}
+        {/if}
+        {#if incoming.length}
+            <CollapsibleSection
+                id="friends_incoming"
+                defaultValue
+                sticky
+                large
+                summary="{$t("app.special.friends.pending")} - {incoming.length}"
+            >
+                <div class="UserGrid">
+                    {#each incoming as user}
+                        <Friend {user} />
+                    {/each}
+                </div>
+            </CollapsibleSection>
+        {/if}
+        {#if outgoing.length}
+            <CollapsibleSection
+                id="friends_outgoing"
+                defaultValue
+                sticky
+                large
+                summary="{$t("app.special.friends.sent")} - {outgoing.length}"
+            >
+                <div class="UserGrid">
+                    {#each outgoing as user}
+                        <Friend {user} />
+                    {/each}
+                </div>
+            </CollapsibleSection>
+        {/if}
+        {#if online.length}
+            <CollapsibleSection
+                id="friends_online"
+                defaultValue
+                sticky
+                large
+                summary="{$t("app.status.online")} - {online.length}"
+            >
+                <div class="UserGrid">
+                    {#each online as user}
+                        <Friend {user} />
+                    {/each}
+                </div>
+            </CollapsibleSection>
+        {/if}
+        {#if offline.length}
+            <CollapsibleSection
+                id="friends_offline"
+                defaultValue
+                sticky
+                large
+                summary="{$t("app.status.offline")} - {offline.length}"
+            >
+                <div class="UserGrid">
+                    {#each offline as user}
+                        <Friend {user} />
+                    {/each}
+                </div>
+            </CollapsibleSection>
+        {/if}
+    </div>
 </div>
 
 <style>
+    .actions {
+        display: flex;
+        align-items: center;
+        gap: 20px;
+    }
+
+    .grid {
+        padding: 0 10px 10px;
+        user-select: none;
+    }
+
+    .grid[data-empty="true"] {
+        gap: 16px;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    .grid[data-mobile="true"] {
+        padding-bottom: var(--bottom-navigation-height);
+    }
+
     .UserGrid {
         display: flex;
         flex-wrap: wrap;
