adf41a3d39030f2379e39f3114bdf214388486f9
New Creative <ianparra200@gmail.com>
2024-04-24 14:43:09 -0400

    feat: show/hide right sidebar

--- src/lib/components/navigation/RightSidebar.svelte
+++ src/lib/components/navigation/RightSidebar.svelte
@@ -1,13 +1,17 @@
 <script>
     import { page } from "$app/stores";
+    import { state } from "$lib/State";
     import { useClient } from "$lib/controllers/ClientController";
+    import { SIDEBAR_MEMBERS } from "$lib/stores/Layout";
+    import { autorun } from "mobx";
     import GroupMemberSidebar from "./right/GroupMemberSidebar.svelte";
     import ServerMemberSidebar from "./right/ServerMemberSidebar.svelte";
 
     let client = useClient();
     $: server_id = $page.params.server;
     $: server = client.servers.get(server_id);
-    let openRight = true;
+    let openRight = state.layout.getSectionState(SIDEBAR_MEMBERS, true);
+    $: autorun(()=> openRight = state.layout.getSectionState(SIDEBAR_MEMBERS, true));
     $: channel_id = $page.params.channel;
     $: channel = client.channels.get(channel_id);
 </script>
