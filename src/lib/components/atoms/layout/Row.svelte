fdaca58f75f30fbe8d67bc2b133f2dabd39ef50a
New Creative <ianparra200@gmail.com>
2024-05-13 02:07:06 -0400

    feat: row component

---
+++ src/lib/components/atoms/layout/Row.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let gap = "8px",
+        centred = false,
+        grow = false;
+</script>
+
+<div
+    class="row"
+    style:gap
+    style:align-items={centred ? "center" : ""}
+    style:flex-grow={grow ? "1" : ""}
+>
+    <slot />
+</div>
+
+<style>
+    .row {
+        display: flex;
+        flex-direction: row;
+    }
+</style>
