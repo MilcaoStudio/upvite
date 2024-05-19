54523755c890e90883cf1997429f03566457d1e8
NewCreative <ianparra200@gmail.com>
2024-05-19 00:55:32 -0400

    fix: background visible and compatible across browsers

--- src/lib/components/atoms/input/HueSlider.svelte
+++ src/lib/components/atoms/input/HueSlider.svelte
@@ -2,18 +2,21 @@
     export let onchange: ((value: number)=>void) | undefined = undefined, value = 0;
 </script>
 
+
 <div class="HueSlider">
-    <input type="range" min="0" max="360" step="1" bind:value on:change={()=>onchange?.(value)} {...$$restProps} />
+    <label><slot/></label>
+    <div>
+        <input type="range" min="0" max="360" step="1" bind:value on:change={()=>onchange?.(value)} {...$$restProps} />
+    </div>
 </div>
 
 <style>
-
-    .HueSlider input {
+    .HueSlider div {
         background: linear-gradient(90deg, hsl(0, 100%, 50%), hsl(45, 100%, 50%), hsl(90, 100%, 50%), hsl(180, 100%, 50%), hsl(270, 100%, 50%),  hsl(360, 100%, 50%));
-        
+        display: inline;
     }
 
-    input::-moz-range-progress {
+    input::-moz-range-progress, input::-ms-track {
         background: transparent;
     }
 </style>
