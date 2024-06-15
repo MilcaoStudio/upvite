308b609ae62a2962ca6460cef8487522a9622558
New Creative <ianparra200@gmail.com>
2024-06-15 00:17:33 -0400

    feat: badge checker

--- scripts/setup_badges.js
+++ scripts/setup_badges.js
@@ -1,36 +1,50 @@
-import { existsSync, readFileSync, } from "fs";
+import { existsSync, createReadStream, } from "fs";
 import axios from "axios";
+import crypto from "crypto";
 import { writeFile } from "fs/promises";
 
 const target = process.env.ASSETS + "/badges";
 const DEFAULT_DIRECTORY = "static/public_badges";
 const OUT_DIRECTORY = "static/badges";
-const files = ["amog.svg", "amorbus.svg", "developer.svg", "early_adopter.svg", "founder.svg", "moderation.svg"]
+
+function createMD5(filePath) {
+    return new Promise((Ok) => {
+        const hash = crypto.createHash("md5");
+
+        const rStream = createReadStream(filePath);
+
+        rStream.on('data', (data) => hash.update(data));
+
+        rStream.on('end', () => Ok(hash.digest('hex')));
+    })
+}
 
 (async () => {
     try {
         await access(OUT_DIRECTORY);
         if (process.argv[2] == "--check") return;
-    } catch (err) {}
+    } catch (err) { }
 
     if (target) {
-        for(const file of files){
-            const target_path = `${target}/${file}`;
+        const content = (await axios(target + "/badges.md5", {responseType: "text"})).data;
+        console.info("> Checksum file fetched from", target + "/badges.md5 :", content);
+        const lines = content.split("\n");
+        for (const line of lines) {
+            const [remote_checksum, file] = line.split(/\s+/);
             const local_path = `${OUT_DIRECTORY}/${file}`;
-            const remote_checksum = (await axios(target_path + ".md5", {responseType: "text"})).data;
-            console.info("> Remote checksum from", target_path + ".md5", ":", remote_checksum);
-            if (existsSync(target_path + ".md5")) {
-                const local_checksum = readFileSync(local_path + ".md5", {encoding: "utf-8"});
-                console.info("> Local checksum located in", local_path + ".md5", ":", local_checksum);
-                if (remote_checksum == local_checksum) {
-                    return;
+            const target_path = `${target}/${file}`;
+            if (existsSync(local_path)) {
+                const local_checksum = await createMD5(local_path);
+                console.info("> Local checksum generated from", local_path, ":", local_checksum);
+                if (local_checksum == remote_checksum) {
+                    console.info("> Success: Checksum match.");
+                    continue;
                 }
-                console.warn("Local checksum and remote checksum are not equal.");
+                console.info("Checksums don't match. Updating icons from ", target_path);
             }
-            writeFile(local_path + ".md5", remote_checksum, {encoding: "utf-8"})
-                .then(()=>console.info("> Checksum updated"));
-            const icon = (await axios(target_path, {responseType: "text"})).data;
+            const icon = (await axios(target_path, { responseType: "text" })).data;
             console.info("> Downloaded icon", icon.slice(0, 30));
+            await writeFile(local_path, icon, { encoding: "utf-8" });
         }
     } else {
         await copy(DEFAULT_DIRECTORY, OUT_DIRECTORY);
