e605c55ffacdac7780a1c7dc22ad835dc5b52081
New Creative <ianparra200@gmail.com>
2024-08-01 19:59:00 -0400

    fix: editor reworked

--- src/lib/components/atoms/input/TextEditor.svelte
+++ src/lib/components/atoms/input/TextEditor.svelte
@@ -3,29 +3,23 @@
         Slate,
         Editable,
         withSvelte,
-        focus,
-        toDOMPoint,
     } from "svelte-slate";
     import {
         createEditor,
-        Text,
-        Element,
-        Transforms,
-        type Descendant,
         type Path,
+        type Range,
+        Text,
+        type Descendant,
         Node,
-        Point
     } from "slate";
     import Leaf from "$lib/markdown/slate/Leaf.svelte";
     import ElementRenderer from "$lib/markdown/slate/ElementRenderer.svelte";
-    import type {
-        KeyboardEventHandler,
-    } from "svelte/elements";
+    import type { KeyboardEventHandler } from "svelte/elements";
     import withMarkdown from "$lib/markdown/withMarkdown";
-    import remarkSlate from "$lib/markdown/plugins/remarkSlate";
-    import { remarkProcessor } from "$lib/markdown/RemarkRenderer.svelte";
     import type { IElement } from "svelte-slate/plugins";
     import { onDestroy, onMount } from "svelte";
+    import { RE_MENTION } from "$lib/markdown/plugins/remarkRegex";
+    import Prism from "$lib/markdown/prism";
     export let value = "",
         minHeight = 0,
         onChange: (
@@ -38,138 +32,84 @@
         onBlur: (() => void) | undefined = undefined;
 
     let ref: HTMLDivElement | undefined;
-    function stringifyNodes(nodes: Descendant[]){
-        return nodes.map(n => Node.string(n)).join('\n')
+    function stringifyNodes(nodes: Descendant[]) {
+        return nodes.map((n) => Node.string(n)).join("\n");
     }
 
-    $: console.log("value", value.length, value);
-    //$: setValue(value);
-
-    function equals(obj1: Descendant, obj2: Descendant): boolean {
-        return (
-            typeof obj1 == typeof obj2 &&
-            (Text.equals(obj1, obj2) ||
-                (Element.isElement(obj1) &&
-                    Element.isElement(obj2) &&
-                    obj1.children == obj2.children))
-        );
-    }
-
-    function mergeTexts(text1: string, text2: string, mode: "longest" = "longest") {
-        let chars: string[] = []
-        if (typeof mode != "string") {
-            throw TypeError("mode should be a string");
-        }
-        if (mode == "longest") {
-            const [longest, shortest] = text1.length > text2.length ? [text1, text2] : [text2, text1];
-            for (let longIndex = longest.length - 1, shortIndex = shortest.length - 1; longIndex >= 0; longIndex--) {
-                const lChar = longest.charAt(longIndex);
-                chars.unshift(lChar);
-                if (shortest.charAt(shortIndex) == lChar) {
-                    if (--shortIndex < 0) {
-                        return longest.slice(0, longIndex) + chars.join("")
-                    }
-                }
-            }
-
-        }
-        return chars.join("");
-    }
-
-    function setValue(_value: string) {
-        console.debug("_value", _value);
-        remarkProcessor()
-            .use(remarkSlate)
-            .process(_value, (err, file) => {
-                if (err) throw err;
-                console.log(
-                    "[unified] Rendered tree",
-                    JSON.stringify(file?.result),
-                );
-                if (Array.isArray(file?.result)) {
-                    const children: IElement[] = file.result;
-                    function mergeNodes(
-                        node1: Descendant,
-                        node2: Descendant,
-                        path: Path,
-                    ) {
-                        if (true) {
-                            if (Text.isText(node1) && Text.isText(node2) && node1.text != node2.text) {
-                                // Overrides current text node
-                                //editor.insertText(mergeTexts(node1.text, node2.text), {at: path});
-                                const dpoint = toDOMPoint(editor, {path, offset: node1.text.length});
-                                console.debug(dpoint);
-                                // Mounted text node has same string length
-                                //dpoint[0].textContent = node2.text;
-                            }
-                            //if (node2.type == "mention") debugger;
-                            //console.log("diff found, path", path);
-                            console.log("Running mergeNodes in", path);
-                            const fn = editor.hasPath(path)
-                                ? Transforms.setNodes
-                                : Transforms.insertNodes;
-                            fn(editor, node2, { at: path, voids: true,  });
-                            if (Element.isElement(node1) && Element.isElement(node2)) {
-                                for (let j = 0; j < Math.max(node1.children.length, node2.children.length); j++) {
-                                    mergeNodes(node1.children[j], node2.children[j], [path[0], j]);
-                                }
-                            }
-                        }
-                    }
-                    for (
-                        let i = 0;
-                        i < Math.max(editor.children.length, children.length);
-                        i++
-                    ) {
-                        mergeNodes(editor.children[i], children[i], [i]);
-                    }
-                    console.debug(
-                        "Actual tree",
-                        JSON.stringify(editor.children),
-                    );
-                    //ASTValue = children;
-                }
-            });
-    }
-
-    /*let ASTValue: IElement[] = [
-        { type: "paragraph", children: [{ text: "" }] },
-    ];*/
     let ASTValue: IElement[] = [
-        {
-            type: "paragraph",
-            children: [{
-                text: "--->"
-            }, {
-                text: "<--"
-            }]
+        { type: "paragraph", children: [{ text: "" }] },
+    ];
+
+    function effect_value(_value: string) {
+        const _ASTValue = [];
+        for (const line of _value.split("\n")) {
+            _ASTValue.push({ type: "paragraph", children: [{ text: line }] });
         }
-    ]
-    $: console.log("AST value", JSON.stringify(ASTValue));
+        ASTValue = _ASTValue;
+    }
+    $: effect_value(value);
+
+    function tokenLen(token: Prism.Token | string): number {
+        return typeof token == "string"
+            ? token.length
+            : typeof token.content == "string"
+              ? token.content.length
+              : Array.isArray(token.content)
+                ? token.content.reduce((l, t) => l + tokenLen(t), 0)
+                : tokenLen(token.content);
+    }
+    function decorate([node, path]: [Node, Path]) {
+        if (!Text.isText(node)) {
+            return [];
+        }
+        const ranges: (Range & { type?: string })[] = [];
+        const tokens = Prism.tokenize(node.text, Prism.languages.markdown);
+
+        let start = 0;
+        for (const token of tokens) {
+            const end = start + tokenLen(token);
+            if (typeof token != "string") {
+                ranges.push({
+                    [token.type]: true,
+                    anchor: { path, offset: start },
+                    focus: { path, offset: end },
+                });
+            }
+            start = end;
+        }
+        const mentions: [string, number][] = (node.text.match(RE_MENTION) ?? [])
+            .map((m) => m.trim())
+            .map((m) => [m, node.text.indexOf(m)]);
+        mentions.forEach(([m, i]) =>
+            ranges.push({
+                anchor: { path, offset: i },
+                focus: { path, offset: i + m.length },
+                type: "mention",
+            }),
+        );
+        return ranges;
+    }
+
     const editor = withMarkdown(withSvelte(createEditor()));
+
     onMount(() => {
         ref?.addEventListener("focus", onFocus);
         onBlur && ref?.addEventListener("blur", onBlur);
     });
+    
     onDestroy(() => {
         ref?.removeEventListener("focus", onFocus);
         onBlur && ref?.removeEventListener("blur", onBlur);
     });
 </script>
 
-<div
-    class="TextEditor"
-    on:focus={(e) => {
-        focus(editor);
-    }}
->
+<div class="TextEditor">
     <Slate
         {editor}
         value={ASTValue}
         on:value={(v) => {
-            console.debug("on:value", JSON.stringify(v.detail));
             const content = stringifyNodes(v.detail);
-            //value = content;
+            editor.move({unit: "line"});
             onChange(
                 content,
                 editor.selection?.anchor.offset,
@@ -182,6 +122,7 @@
                 bind:ref
                 {Leaf}
                 Element={ElementRenderer}
+                {decorate}
                 placeholder=""
                 autoFocus
                 onKeyDown={(e) => {
