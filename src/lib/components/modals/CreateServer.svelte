2b5c4becfd4d43bf539207e081e4ebe3f6dc73c6
New Creative <ianparra200@gmail.com>
2024-08-14 23:26:49 -0400

    fix: create server modal activates SSR mode

--- src/lib/components/modals/CreateServer.svelte
+++ src/lib/components/modals/CreateServer.svelte
@@ -2,8 +2,7 @@
     import { useClient } from "$lib/controllers/ClientController";
     import type { ModalProps } from "$lib/types/Modal";
     import { _ } from "svelte-i18n";
-    import ModalForm from "./ModalForm.svelte";
-    import { pushState } from "$app/navigation";
+    import { goto, } from "$app/navigation";
     import { mapError } from "$lib";
     import DialogForm from "./DialogForm.svelte";
 
@@ -12,36 +11,13 @@
     const client = useClient();
 
 </script>
-<!--
-<ModalForm
-    props={props}
-    callback={async ({name})=>{
-        const server = await client.servers.createServer({
-            name: ''+name,
-        }).catch(mapError);
-        pushState(`/server/${server?._id}`, {})
-    }}
-    schema={{name: "text"}}
-    data={{name: {field: $_('app.main.servers.name')}}}
-    submit={{children: $_('app.special.modals.actions.create')}} >
-    <svelte:fragment slot="title">{$_('app.main.servers.create')}</svelte:fragment>
-    <div slot="description">
-        By creating this server, you agree to the{" "}
-        <a
-            href="http://localhost"
-            target="_blank"
-            rel="noreferrer">
-                Acceptable Use Policy.
-        </a>
-    </div>
-</ModalForm>
--->
+
 <DialogForm {...props}
     callback={async ({name})=>{
         const server = await client.servers.createServer({
         name: ''+name,
         }).catch(mapError);
-        pushState(`/server/${server?._id}`, {})
+        goto(`/server/${server?._id}`,);
     }}
     schema={{name: "text"}}
     data={{name: {field: $_('app.main.servers.name')}}}
