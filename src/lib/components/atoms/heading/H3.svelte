726ebbf2ca10a1842441c8481c699d58b72da2b0
New Creative <ianparra200@gmail.com>
2024-08-18 18:57:58 -0400

    style: h3 with margin

--- src/lib/components/atoms/heading/H3.svelte
+++ src/lib/components/atoms/heading/H3.svelte
@@ -3,15 +3,11 @@
        NOT TO BE CONFUSED WITH CATEGORY TITLES, 
        DO NOT USE HEADINGS ON THOSE. */
     h3 {
-       font-size: 0.75rem;
+        font-size: 0.75rem;
         font-weight: 700;
         text-transform: uppercase;
         color: var(--secondary-foreground);
     }
-
-    h3:first-child {
-        margin-top: 0;
-    }
 </style>
 
-<h3><slot /></h3>
+<h3 {...$$restProps}><slot /></h3>
